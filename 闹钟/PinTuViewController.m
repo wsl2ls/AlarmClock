01b5c71173494396c7fa2b63a8e57eb46331d719
wsl <15324956576@163.com>
2016-07-01 10:28:56 +0800

    动画

--- 闹钟/PinTuViewController.m
+++ 闹钟/PinTuViewController.m
@@ -11,6 +11,7 @@
 #define kSCREEN_FRAME ([UIScreen mainScreen].applicationFrame)       // 屏幕Frame
 #define kSCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)      // 屏幕宽度
 #define kSCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)    // 屏幕高度
+#define kBounds [UIScreen mainScreen].bounds.size
 
 @interface PinTuViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
@@ -615,6 +616,43 @@
     UIGraphicsEndImageContext();
     
     return smallImage;
+}
+#pragma mark --- 点赞动画
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self praise];
+}
+
+- (void)praise{
+    UIImageView *imageView = [[UIImageView alloc] init];
+    imageView.frame = CGRectMake(kBounds.width - 60, kBounds.height - 49, 35, 35);
+    imageView.image = [UIImage imageNamed:@"heart"];
+    imageView.backgroundColor = [UIColor clearColor];
+    imageView.clipsToBounds = YES;
+    [self.view addSubview:imageView];
+    
+    
+    CGFloat startX = round(random() % 200);
+    CGFloat scale = round(random() % 2) + 1.0;
+    CGFloat speed = 1 / round(random() % 900) + 0.6;
+    int imageName = round(random() % 12);
+    NSLog(@"%.2f - %.2f -- %d",startX,scale,imageName);
+    
+    [UIView beginAnimations:nil context:(__bridge void *_Nullable)(imageView)];
+    [UIView setAnimationDuration:7 * speed];
+    
+    imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"resource.bundle/heart%d.png",imageName]];
+    imageView.frame = CGRectMake(kBounds.width - startX, -100, 35 * scale, 35 * scale);
+    
+    [UIView setAnimationDidStopSelector:@selector(onAnimationComplete:finished:context:)];
+    [UIView setAnimationDelegate:self];
+    [UIView commitAnimations];
+}
+
+
+- (void)onAnimationComplete:(NSString *)animationID finished:(NSNumber *)finished context:(void *)context{
+    
+    UIImageView *imageView = (__bridge UIImageView *)(context);
+    [imageView removeFromSuperview];
 }
 
 - (void)didReceiveMemoryWarning {
